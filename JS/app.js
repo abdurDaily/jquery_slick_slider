44a1866dfb14f1579da774e3da4ff13d52853e2f
abdurDaily <abdurdaily@gmail.com>
2024-01-29 17:39:53 +0600

    slick slider

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: ".right",
+        nextArrow: ".left",
+        centerMode: true,
+        centerPadding: '0px',
+    });
+})
